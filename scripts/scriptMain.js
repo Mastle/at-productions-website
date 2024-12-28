b78f408d2a8e82f8fb877d40a215b698a1d15184
Mastle <amirali.tsh13@gmail.com>
2024-12-28 19:46:04 +0330

    removed swiper, upgrading projects

--- scripts/scriptMain.js
+++ scripts/scriptMain.js
@@ -7,37 +7,3 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });   
 
-
-
-
-//Slider options
-const swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
-
-
-//Moving the slider
- setInterval(()=> document.getElementById('swiper-right-handle').click(), 3000)
-
-
